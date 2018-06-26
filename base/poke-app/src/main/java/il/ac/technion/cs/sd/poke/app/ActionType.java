11b4b747064e642f2c60a5b304b6ae2c3f48fdf5
Shadi Kardosh <shadikardosh@campus.technion.ac.il>
2018-06-26 14:12:58 +0300

    get for actionType

--- base/poke-app/src/main/java/il/ac/technion/cs/sd/poke/app/ActionType.java
+++ base/poke-app/src/main/java/il/ac/technion/cs/sd/poke/app/ActionType.java
@@ -1,5 +1,12 @@
 package il.ac.technion.cs.sd.poke.app;
 
 public enum ActionType {
-    POKEMON, CATCH, RELEASE
+    POKEMON, CATCH, RELEASE;
+    static public ActionType get(String s){
+        if (s.equals("catch"))
+            return CATCH;
+        if (s.equals("pokemon"))
+            return POKEMON;
+        return RELEASE;
+    }
 }
